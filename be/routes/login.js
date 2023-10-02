e2b623d4dc7200ec5163f6e5767146ac607ed17b
CodingLukas <2143609+CodingLukas@users.noreply.github.com>
2023-10-02 06:59:27 +0300

    fixes

--- be/routes/login.js
+++ be/routes/login.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const admin = require('firebase-admin');
 const jwt = require('jsonwebtoken');
 const db = admin.firestore();
